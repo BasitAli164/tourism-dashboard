54e4bcf1d4a4a0c3d46a19791e40d02aa911d37c
Basit Ali <opoali911@gmail.com>
2025-12-12 21:29:19 +0500

    fix some issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
       <footer className="bg-white shadow-md py-3 sm:py-4 px-6 text-center">
         <p className="text-[13px] sm:text-sm text-gray-600">
-          © Mountain Travels Pakistan Admin-Dashboard ♥ crafted by Team Net Bots(SMC-Private)Limited
+          © Mountain Travels Pakistan Admin-Dashboard ♥ crafted by Basit Ali
         </p>
       </footer>
     )
